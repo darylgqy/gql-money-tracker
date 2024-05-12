c7721295fc72905f152663daab04ac003336bfb9
Daryl Goh <darylgqy@gmail.com>
2024-05-13 01:35:30 +0800

    backend gql and mongodb setup

---
+++ backend/typeDefs/transaction.typeDef.js
@@ -0,0 +1,47 @@
+import { defaultTypeResolver } from "graphql"
+
+const transactionTypeDef = `#graphql
+    type Transaction {
+        _id: ID!
+        userId: ID!
+        description: String!
+        paymentType: String!
+        category: String!
+        amount: Float!
+        location: String
+        date: String!
+    }
+
+    type Query {
+        transactions: [Transaction!] #fetch all
+        transaction(transactionId:ID!): Transaction #fetch just 1 transaction
+    }
+
+    type Mutation { #possible CRUD actions on the transactions
+        createTransactions(input: CreateTransactionInput!): Transaction
+        updateTransaction(input: UpdateTransactionInput!): Transaction
+        deleteTransaction(transactionId:ID!): Transaction!
+    }
+
+    input CreateTransactionInput {
+        description: String!
+        paymentType: String!
+        category: String!
+        amount: Float!
+        date: String!
+        location: String
+    }
+
+    input UpdateTransactionInput {
+        transactionId: ID!
+        description: String
+        paymentType: String
+        category: String
+        amount: Float
+        location: String
+        date: String
+    }
+
+`
+
+export default transactionTypeDef;
